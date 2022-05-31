870af9c3f596ef1d07fdfb373ecf6187f49c3550
Ananya Butwong <ananya.butwong@gmail.com>
2022-05-31 10:51:40 +0700

    T10 | Trik

---
+++ T10/README.md
@@ -0,0 +1,14 @@
+# Trik
+## Definition
+has 3 glass, ball start at left
+- A: swap left to mid or mid to left
+- B: swap mid to right or right to mid
+- C: swap left to right or right to left
+
+## Input
+- single line with chacters (A, B, or C) that lenght < 50
+
+## Output
+- if ball at left, print '1'
+- if ball at mid, print '2'
+- if ball at right, print '3'
---
+++ T10/t10.py
@@ -0,0 +1,21 @@
+cha = input()
+ball = 1
+
+for i in cha:
+    if i == 'A':
+        if ball == 1:
+            ball = 2
+        elif ball == 2:
+            ball = 1
+    elif i == 'B':
+        if ball == 2:
+            ball = 3
+        elif ball == 3:
+            ball = 2
+    elif i == 'C':
+        if ball == 1:
+            ball = 3
+        elif ball == 3:
+            ball = 1
+            
+print(ball)
